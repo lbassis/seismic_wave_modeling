5a711c474a8edf69afc50e2309b9c13deb1df5f7
Lucas <lbassis@inf.ufrgs.br>
2021-02-25 22:50:43 -0300

    stencil's naive version without blocking the matrix

--- lucas/demo_stencil/starpu_stencil.c
+++ lucas/demo_stencil/starpu_stencil.c
@@ -4,7 +4,8 @@
 #include <time.h>
 #include <starpu.h>
 
-#define SIZE 10 // 9x9 + padding
+#define SIZE 5
+//#define BLOCK_SIZE 3
 #define EPSILON 0.001
 #define STENCIL_MAX_STEPS 10000
 
@@ -18,38 +19,37 @@ static void stencil_init(float **prev_vector, float **next_vector) {
   int i, j;
   for(i = 0; i < SIZE; i++) {
     for(j = 0; j < SIZE; j++) {
-      (*prev_vector)[i*n+j] = 0.0;
-      (*next_vector)[i*n+j] = 0.0;
+      (*prev_vector)[i*SIZE+j] = 0.0;
+      (*next_vector)[i*SIZE+j] = 0.0;
     }
   }
   for(i = 0; i < SIZE; i++) {
-    (*prev_vector)[i*n] = i;
-    (*prev_vector)[i*n+n-1] = SIZE - i;
-    (*next_vector)[i*n] = i;
-    (*next_vector)[i*n+n-1] = SIZE - i;
+    (*prev_vector)[i*SIZE] = i;
+    (*prev_vector)[i*SIZE+SIZE-1] = SIZE - i;
+    (*next_vector)[i*SIZE] = i;
+    (*next_vector)[i*SIZE+SIZE-1] = SIZE - i;
 
   }
   for(j = 0; j < SIZE; j++) {
     (*prev_vector)[j] = j;
-    (*prev_vector)[(n-1)*n+j] = SIZE - j;
+    (*prev_vector)[(SIZE-1)*SIZE+j] = SIZE - j;
     (*next_vector)[j] = j;
-    (*next_vector)[(n-1)*n+j] = SIZE - j;
+    (*next_vector)[(SIZE-1)*SIZE+j] = SIZE - j;
   }
 }
 
 /** return 1 if computation has converged */
 static int stencil_test_convergence(int current_buffer, float *prev_vector, float *next_vector) {
-  int prev_buffer = (current_buffer - 1 + STENCIL_NBUFFERS) % STENCIL_NBUFFERS;
   int i, j;
   for(i = 1; i < SIZE-1; i++) {
     for(j = 1; j < SIZE-1; j++) {
       if (current_buffer == 0) {
-	if(fabs(prev_vector[i*n+j] - next_vector[i*n+j]) > EPSILON) {
+	if(fabs(prev_vector[i*SIZE+j] - next_vector[i*SIZE+j]) > EPSILON) {
 	  return 0;
 	}
       }
       else {
-	if(fabs(next_vector[i*n+j] - prev_vector[i*n+j]) > EPSILON) {
+	if(fabs(next_vector[i*SIZE+j] - prev_vector[i*SIZE+j]) > EPSILON) {
 	  return 0;
 	}		
       }
@@ -60,10 +60,10 @@ static int stencil_test_convergence(int current_buffer, float *prev_vector, floa
 
 /** display a part of the stencil values */
 static void stencil_display(float *vector, int x0, int x1, int y0, int y1) {
-  int i, x;
+  int i, j;
   for(i = y0; i <= y1; i++) {
     for(j = x0; j <= x1; j++) {
-      printf("%8.5g ", vector[i*n+j]);
+      printf("%8.5g ", vector[i*SIZE+j]);
     }
     printf("\n");
   }
@@ -74,17 +74,11 @@ static void stencil_display(float *vector, int x0, int x1, int y0, int y1) {
 /* --- StarPU functions --- */
 /* =========================== */
 
-struct starpu_codelet stencil_step = {
-				      .cpu_funcs = {step_func, NULL},
-				      .nbuffers = 3, // alpha, prev_vector, next_vector
-				      .modes = {STARPU_R, STARPU_R, STARPU_W},
-};
-
 void step_func(void *buffers[], void *cl_arg) {
   struct starpu_vector_interface *prev_vector_handle = buffers[0];
   struct starpu_vector_interface *next_vector_handle = buffers[1];
 
-  unsigned n = STARPU_VECTOR_GET_NX(prev_vector_handle);
+  //unsigned n = STARPU_VECTOR_GET_NX(prev_vector_handle);
   
   float *prev_vector = (float *)STARPU_VECTOR_GET_PTR(prev_vector_handle);
   float *next_vector = (float *)STARPU_VECTOR_GET_PTR(next_vector_handle);
@@ -92,18 +86,23 @@ void step_func(void *buffers[], void *cl_arg) {
   
   starpu_codelet_unpack_args(cl_arg, &alpha);
 
-  for (int i = 1; i < n-1; i++) {
-    for (int j = 1; j < n-1; j++) {
-      next_vector[i*n+j] =
-	alpha * prev_vector[(i-1)*n+j] +
-	alpha * prev_vector[(i+1)*n+j] +
-	alpha * prev_vector[i*n+j-1] +
-	alpha * prev_vector[i*n+j+1] +
-	(1.0 - 4.0 * alpha) * prev_vector[i*n+j];
+  for (int i = 1; i < SIZE-1; i++) {
+    for (int j = 1; j < SIZE-1; j++) {
+    next_vector[i*SIZE+j] =
+	alpha * prev_vector[(i-1)*SIZE+j] +
+	alpha * prev_vector[(i+1)*SIZE+j] +
+	alpha * prev_vector[i*SIZE+j-1] +
+	alpha * prev_vector[i*SIZE+j+1] +
+	(1.0 - 4.0 * alpha) * prev_vector[i*SIZE+j];
     }
   }
 }
 
+struct starpu_codelet stencil_step = {
+				      .cpu_funcs = {step_func},
+				      .nbuffers = 2, // prev_vector, next_vector
+				      .modes = {STARPU_R, STARPU_W},
+};
 
 /* =========================== */
 /* ---         Main        --- */
@@ -112,10 +111,11 @@ void step_func(void *buffers[], void *cl_arg) {
 int main(int argc, char**argv) {
 
   int current_buffer = 0;
-  double alpha = 0.02;
-  float prev_vector[SIZE*SIZE], next_vector[SIZE*SIZE];
+  float alpha = 0.02;
+  float *prev_vector = malloc(sizeof(float)*SIZE*SIZE);
+  float *next_vector = malloc(sizeof(float)*SIZE*SIZE);
   starpu_data_handle_t prev_vector_handle, next_vector_handle;
-
+    
   int ret = starpu_init(NULL);
   if (ret != 0) {
     printf("Erro inicializando StarPU\n");
@@ -123,27 +123,38 @@ int main(int argc, char**argv) {
   }
 
   stencil_init(&prev_vector, &next_vector);
+  stencil_display(prev_vector, 0, SIZE-1, 0, SIZE-1);
 
-  // actually we will need one handle for each part ie 9 handles for each 3x3 block for each vector
-  starpu_vector_data_register(&prev_vector_handle, 0, (uintptr_t)prev_vector, SIZE, sizeof(prev_vector[0]));
-  starpu_vector_data_register(&next_vector_handle, 0, (uintptr_t)next_vector, SIZE, sizeof(next_vector[0]));
+ 
+  starpu_vector_data_register(&prev_vector_handle, 0, (uintptr_t)prev_vector, SIZE*SIZE, sizeof(prev_vector[0]));
+  starpu_vector_data_register(&next_vector_handle, 0, (uintptr_t)next_vector, SIZE*SIZE, sizeof(next_vector[0]));
 
   for(int s = 0; s < STENCIL_MAX_STEPS; s++) {
-    // 9 tasks for treating 3x3
 
     int prev_buffer = current_buffer;
     int next_buffer = (current_buffer + 1) % 2;
 
+    printf("step %d\n", s);
     // the handles will depend on the current buffer
+    if (current_buffer == 0) {
       starpu_insert_task(&stencil_step,
 			 STARPU_R, prev_vector_handle,
 			 STARPU_W, next_vector_handle,
 			 STARPU_VALUE, &alpha, sizeof(alpha),
 			 0);
+    }
+    else {
+      starpu_insert_task(&stencil_step,
+			 STARPU_R, next_vector_handle,
+			 STARPU_W, prev_vector_handle,
+			 STARPU_VALUE, &alpha, sizeof(alpha),
+			 0);
+    }
+
       starpu_task_wait_for_all();
-      if(stencil_test_convergence()) {
-	  //printf("# steps = %d\n", s);
-	  break;
+      if(stencil_test_convergence(current_buffer, prev_vector, next_vector)) {
+      	  printf("# steps = %d\n", s);
+      	  break;
       }
 
       current_buffer = next_buffer;
@@ -154,7 +165,14 @@ int main(int argc, char**argv) {
 
   starpu_shutdown();
 
-  //stencil_display(current_buffer, 0, n-1, 0, n-1);
+  printf("result:\n");
+  if (current_buffer == 0) {
+      stencil_display(next_vector, 0, SIZE-1, 0, SIZE-1);
+  }
+  else {
+    stencil_display(prev_vector, 0, SIZE-1, 0, SIZE-1);
+  }
+
   return 0;
 }
 
